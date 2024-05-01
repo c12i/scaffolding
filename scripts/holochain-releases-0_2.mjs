53a133d7729ab8776aa1bcbd53b608625516f998
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-05-01 17:33:57 +0300

    add release check cron

---
+++ scripts/holochain-releases-0_2.mjs
@@ -0,0 +1,37 @@
+import https from 'node:https'
+
+const URL = 'https://api.github.com/repos/holochain/holochain/releases'
+const request = {
+		headers: {'User-Agent': 'scaffolding-holochain-releases'},
+}
+
+function n_hours_ago(n) {
+	return new Date(new Date().getTime() - (n * 60 * 60 * 1000))
+}
+
+https.get(URL, request, (res) => {
+	let data = '';
+
+	res.on('data', (chunk) => {
+			data += chunk;
+	});
+
+	res.on('end', () => {
+			try {
+					const result = JSON.parse(data);
+					const holochain_0_2 = result
+						.filter(r => r.tag_name.startsWith('holochain-0.2'))
+						.filter(r => new Date(r.published_at) >= n_hours_ago(6))
+						.map(r => r.tag_name)
+					
+					if (holochain_0_2.length) {
+						console.log(holochain_0_2[0])
+					}
+			} catch (e) {
+					console.error('Error processing result', e);
+			}
+	});
+
+}).on('error', (e) => {
+	console.error(`Got error: ${e.message}`);
+});
